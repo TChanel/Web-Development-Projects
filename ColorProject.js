1656dc4cc755bdb4db9c7e3b40c6a245d4ab7a7c
TChanel <tchanel.hammond@gmail.com>
2020-06-13 16:04:03 -0400

    reset button implemented

--- ColorProject.js
+++ ColorProject.js
@@ -1,9 +1,24 @@
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+    //generate new colors
+    colors = generateRandomColors(6);
+    //pick a new rnadom color from the array
+    pickedColor = pickColor();
+    //change colorDIspplay to math match picked Color
+    colorDisplay.textContent = pickedColor;
+    //change colors of squares
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+    }
+    h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,7 +32,9 @@ for(var i = 0; i < squares.length; i++){
         console.log(clickedColor, pickedColor);
         if (clickedColor === pickedColor){
             messageDisplay.textContent = "Correct."
+            resetButton.textContent = "Play again."
             changeColors(clickedColor);
+            h1.style.background = clickedColor;
         } 
         else {
             this.style.background = "#232323";
