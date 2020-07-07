642cefff484d90668fb91781cb10f980963e3e66
TChanel <tchanel.hammond@gmail.com>
2020-07-07 17:54:30 -0400

    finished backend +started css

--- todo.js
+++ todo.js
@@ -1,11 +1,24 @@
 //checkk off specific todo by clciing
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
     
 });
-//click on Xto delete todo
-$("span").click(function(event){
-    console.log("clicked o the span");
+//click on X to delete todo
+$("span").on("click","span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
     event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        //add new todo
+        $("ul").append("<li><span>X </span>" + todoText +  "</li>")
+    }
 })
+
+
