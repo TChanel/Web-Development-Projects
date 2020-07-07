5fd7c0cca58d2e0a95d22c42c6634174eb5ce249
TChanel <tchanel.hammond@gmail.com>
2020-07-07 01:13:01 -0400

    working on delete button

--- todo.js
+++ todo.js
@@ -4,3 +4,8 @@ $("li").click(function(){
     $(this).toggleClass("completed");
     
 });
+//click on Xto delete todo
+$("span").click(function(event){
+    console.log("clicked o the span");
+    event.stopPropagation();
+})
